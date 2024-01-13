52a619dac6a2497791d035f81ea28f0fa8521a14
Keziaeci <keziaeci2806@gmail.com>
2024-01-13 15:04:04 +0700

    Initializing Repo

---
+++ database/migrations/2024_01_05_051337_create_bukus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bukus', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->integer('tahun_terbit');
+            $table->foreignId('penerbit_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('pengarang_id')->constrained()->cascadeOnDelete();
+            $table->integer('stok')->nullable();
+            $table->string('cover');
+            $table->text('deskripsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bukus');
+    }
+};
