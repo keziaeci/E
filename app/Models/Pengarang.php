5272a6f254f09e814479513133ead57d4f596670
Keziaeci <keziaeci2806@gmail.com>
2024-01-13 15:57:18 +0700

    feat: add relations

--- app/Models/Pengarang.php
+++ app/Models/Pengarang.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Pengarang extends Model
 {
     use HasFactory;
+
+    function bukus() : HasMany {
+        return $this->hasMany(Buku::class);
+    }
 }
